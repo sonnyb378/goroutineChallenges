c2c45a94c4bd2738e369c00a96a276aaea1eeb66
Sonny Baga <sonnybaga@Sonnys-Mac-mini.lan>
2023-10-20 16:06:42 -0700

    feat(challenge-1): Implement SumAllElements functionality

---
+++ README.md
@@ -0,0 +1,13 @@
+# Goroutine Project Challenges
+
+This repository is a collection of challenges aimed at helping me understand Goroutines, Channels, WaitGroup, and Mutexes in GoLang. By working on these projects, I have gained practical experience and insight into concurrent programming in Go.
+
+## Things I've Learned
+
+During this project, I've gained valuable insights into concurrent programming in GoLang, including:
+
+- How to create and manage Goroutines effectively.
+- Using channels for inter-Goroutine communication.
+- Synchronizing Goroutines using WaitGroups for orderly execution.
+- Protecting shared resources with Mutexes to avoid data races.
+- Understanding deadlock errors
--- challenges/easy/SumAllElements.go
+++ challenges/easy/SumAllElements.go
@@ -2,6 +2,8 @@ package easy
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
 func SumAllElements(input []int) (string, error) {
@@ -16,6 +18,53 @@ func SumAllElements(input []int) (string, error) {
 	// Desired Output:
 	// Sum: 55
 
+	wg := sync.WaitGroup{}
+
+	numberOfGoroutines := 3
+	chunkSize := len(input) / numberOfGoroutines
 	var allSum int
+	partialSum := make(chan int)
+
+	timeStart := time.Now()
+
+	for i := 0; i < numberOfGoroutines; i++ {
+
+		start := i * chunkSize
+		end := (i + 1) * chunkSize
+
+		// For the last chunk, include any remaining elements
+		if i == numberOfGoroutines-1 {
+			end = len(input)
+		}
+
+		wg.Add(1)
+		go SumAll(&wg, partialSum, input[start:end])
+	}
+
+	go func() {
+		wg.Wait()
+		close(partialSum)
+	}()
+
+	// Receive partialSum channel value
+	for v := range partialSum {
+		allSum += v
+		time.Sleep(time.Second * 1)
+	}
+
+	elapsedTime := time.Since(timeStart)
+	fmt.Println("Elapsed Time: ", elapsedTime.Round(time.Millisecond))
 	return fmt.Sprintf("Sum: %v\n", allSum), nil
+}
+
+func SumAll(wg *sync.WaitGroup, ps chan int, input []int) {
+
+	defer wg.Done()
+
+	sum := 0
+	for _, v := range input {
+		sum += v
+	}
+	ps <- sum
+	fmt.Println("Partial Sum Computation: ", sum)
 }
